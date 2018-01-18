d5fc36112e1a4ccc0a4df5bf5af6e2d69a73859d
Daniel Kraus <daniel@kraus.my>
2018-01-18 17:04:35 +0800

    Move variables out from custom.el to init file

--- etc/custom.el
+++ etc/custom.el
@@ -3,14 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(comint-buffer-maximum-size 5000)
- '(custom-safe-themes
-   '("7a8a552ce273b401673ab786cc2a1dd9391f94e79388fc29164fc393026cb7b7" "b34b5c378458d0a2bd43adbbeda16792fbd09273aed0d1aad5a2e8de17f4f6ae" "b9a06c75084a7744b8a38cb48bc987de10d68f0317697ccbd894b2d0aca06d2b" "a19265ef7ecc16ac4579abb1635fd4e3e1185dcacbc01b7a43cf7ad107c27ced" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default))
  '(safe-local-variable-values
    '((eval web-mode-set-engine "django")
-     (fmt-inhibit-p)
-     (python-test-backend . django)))
- '(sml/name-width '(12 . 42)))
+     (fmt-inhibit-p))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
