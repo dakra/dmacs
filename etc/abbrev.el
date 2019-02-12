8f530bcc60b2f5ec5bf9ab55c1159ba1d5702201
Daniel Kraus <daniel@kraus.my>
2019-02-12 10:18:54 +0000

    Configure abbrev-mode to convert Umlauts

---
+++ etc/abbrev.el
@@ -0,0 +1,26 @@
+;;-*-coding: utf-8;-*-
+(define-abbrev-table 'global-abbrev-table
+  '(
+    ("angehaengt" "angehängt" nil :count 1)
+    ("ausgefuellt" "ausgefüllt" nil :count 0)
+    ("ausgefuelltes" "ausgefülltes" nil :count 0)
+    ("benoetige" "benötige" nil :count 0)
+    ("benoetigt" "benötigt" nil :count 0)
+    ("bloed" "blöd" nil :count 0)
+    ("duenn" "dünn" nil :count 0)
+    ("erklaerte" "erklärte" nil :count 0)
+    ("frueher" "früher" nil :count 0)
+    ("fuer" "für" nil :count 0)
+    ("geklaert" "geklärt" nil :count 1)
+    ("gruesse" "Grüße" nil :count 1)
+    ("gruss" "Gruß" nil :count 2)
+    ("haengt" "hängt" nil :count 2)
+    ("identitaet" "Identität" nil :count 0)
+    ("koenntet" "könntet" nil :count 3)
+    ("natuerlich" "natürlich" nil :count 1)
+    ("rueckruf" "Rückruf" nil :count 0)
+    ("schoen" "schön" nil :count 2)
+    ("ueben" "üben" nil :count 0)
+    ("waere" "wäre" nil :count 5)
+   ))
+
