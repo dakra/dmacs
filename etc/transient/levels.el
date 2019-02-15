99e7505f9d2beecd250eefe798f142166c271bf9
Daniel Kraus <daniel@kraus.my>
2019-02-15 15:29:01 +0000

    Switch from magit-popup to transient
    Update forge     to v0.1.0-66-g5032fb6
    Update magit     to v2.90.1-240-g73a100d4
    Assimilate transient v0.1.0-3-g44fa23c

---
+++ etc/transient/levels.el
@@ -0,0 +1,9 @@
+((magit-commit
+  (magit:--gpg-sign . 4)
+  (magit-commit-absorb . 4)
+  (magit-commit-reshelve . 4))
+ (magit-file-dispatch
+  (magit-file-checkout . 4)
+  (magit-file-untrack . 4)
+  (magit-file-delete . 4)
+  (magit-file-rename . 4)))
