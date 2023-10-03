ed98b23a11cefbcf90c0671692d91f5dd768b5cf
Daniel Kraus <daniel@kraus.my>
2023-10-03 10:18:35 +0200

    Update 46 drones
    Update annotate             to  1.4.3-118-geae9e73
    Update apheleia             to v3.2-43-g970b331
    Update atomic-chrome        to v2.0.0-41-g072a137
    Update auctex               to release_13_2-71-gcb0e671b
    Update browse-at-remote     to  0.14.0-48-ge6f3280
    Update cider                to v1.7.0-152-g0c1e1b1f
    Update company              to  0.9.13-394-gf3ee625
    Update compat               to  29.1.4.2-23-g6571f83
    Update consult              to  0.35-34-ge269bc3
    Update datetime             to  0.9.1-2-gdc905e6
    Update disk-usage           to  1.3.3-11-gb0d803f
    Update docker               to  2.2.0-27-gce5ccdd
    Update editorconfig         to v0.10.1-8-g4bf871b
    Update elisp-refs           to  1.5-7-g541a064
    Update embark               to  0.23-9-gfcf068c
    Update eshell-prompt-extras to  1.1-5-g10dc869
    Update forge                to v0.3.2-182-gadce847
    Update git-modes            to  1.4.1-1-g3889534
    Update haskell-mode         to v17.4-14-g0207074
    Update inf-ruby             to  2.5.2-70-g6191ca2
    Update irony                to v1.6.0
    Update jinx                 to  0.9-39-gf5467b6
    Update ledger-mode          to v4.0.0-188-g105d6762
    Update logview              to  0.17.1-1-g00b3b01
    Update lsp-java             to  3.1-76-g449673d
    Update lsp-metals           to  1.2.0-63-g81002e4
    Update magit                to v3.3.0-788-g19b85820
    Update marginalia           to  1.3-8-g4e14bc0
    Update markdown-mode        to v2.6-4-gc12adcf
    Update modus-themes         to  4.3.0-6-g2844336
    Update mu4e                 to v1.10.0-356-gee18c027
    Update neil                 to v0.2.61-1-gd76462a
    Update nim-mode             to v0.4.1-99-g674ecd1
    Update orderless            to  1.0-14-gd6b402a
    Update org                  to release_9.6.9-801-g0ed8bf794
    Update org-contrib          to release_0.4.2-26-gfd232df
    Update org-ql               to v0.7.2-9-gab72068
    Update org-super-agenda     to v1.3-6-g4efc0bd
    Update php-mode             to v1.25.0-34-g4a29636
    Update sly                  to  1.0.43-82-g9d613cf1
    Update speed-type           to  1.3-6-gb982ee6
    Update sql-indent           to v1.6-10-g323ece6
    Update treemacs             to  3.1-44-g42b9e48
    Update vertico              to  1.4-30-g3baad81
    Update vundo                to v2.1.0-6-g56c0863
    Update yaml                 to v0.5.4

--- etc/transient/values.el
+++ etc/transient/values.el
@@ -1,1 +1,2 @@
-((magit-log:magit-log-mode "-n256" "--graph" "--color" "--decorate"))
+((magit-log:magit-log-mode "-n256" "--graph" "--color" "--decorate")
+ (magit-log:magit-status-mode "--decorate"))
